50ffc3e92474a3467cf102870793908ad21b6452
DaRwin <darekaze@gmail.com>
2022-03-09 19:56:08 +0800

    remove python 3.10 test after fix

--- gatered/__init__.py
+++ gatered/__init__.py
@@ -10,5 +10,5 @@ __version__ = "0.1.0"
 # Import error classes from httpx: https://www.python-httpx.org/exceptions/
 from httpx import RequestError, HTTPStatusError
 
-from .client import Client
+from .client import Client, PushShiftAPI
 from .func import get_posts, get_post_comments

d910e0b996570ac9205461131bbdf4586449e780
DaRwin <darekaze@gmail.com>
2022-03-09 16:35:54 +0800

    feat: export request errors class

--- gatered/__init__.py
+++ gatered/__init__.py
@@ -7,5 +7,8 @@ A utils for interacting with Reddit Gateway API (Web API).
 
 __version__ = "0.1.0"
 
+# Import error classes from httpx: https://www.python-httpx.org/exceptions/
+from httpx import RequestError, HTTPStatusError
+
 from .client import Client
 from .func import get_posts, get_post_comments

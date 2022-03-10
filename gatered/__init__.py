dda62ab7f441bfeb4c16a7302c817e35eed5b3fb
DaRwin <darekaze@gmail.com>
2022-03-10 20:39:14 +0800

    add: export functions

--- gatered/__init__.py
+++ gatered/__init__.py
@@ -12,4 +12,4 @@ from httpx import RequestError, HTTPStatusError
 
 from .client import Client
 from .pushshift import PushShiftAPI
-from .func import get_post_comments
+from .func import get_post_comments, get_posts, get_pushshift_posts

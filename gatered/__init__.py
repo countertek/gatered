b6befc2129e95bcbfc5c2bdb1d197f868b6ec457
DaRwin <darekaze@gmail.com>
2022-03-15 21:10:30 +0800

    bump 0.3.0 -> 0.3.1

--- gatered/__init__.py
+++ gatered/__init__.py
@@ -5,7 +5,7 @@ gatered
 A utils for interacting with Reddit Gateway API (Web API).
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 # Import error classes from httpx: https://www.python-httpx.org/exceptions/
 from httpx import RequestError, HTTPStatusError

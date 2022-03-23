518b292538f19f7639e4bf582b9c99156c4eb76b
DaRwin <darekaze@gmail.com>
2022-03-23 19:55:35 +0800

    fix: fetch comments example

--- examples/fetch_comments.py
+++ examples/fetch_comments.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 
 async def fetch_comments(submission_id: str):
-    async for comments in get_comments(submission_id, all_comments=False):
+    async for comments in get_comments(submission_id):
         pprint(comments[0])
         pprint(len(comments))
 
@@ -12,7 +12,7 @@ async def fetch_comments(submission_id: str):
 async def fetch_comments_aggregate(submission_id: str):
     res = [
         comment
-        async for comments in get_comments(submission_id, all_comments=True)
+        async for comments in get_comments(submission_id)
         for comment in comments
     ]
     pprint(len(res))
@@ -20,4 +20,4 @@ async def fetch_comments_aggregate(submission_id: str):
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(fetch_comments("t3_t97ji9"))
+    loop.run_until_complete(fetch_comments("t3_tjrn4k"))
